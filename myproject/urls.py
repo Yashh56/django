a5b80a829cad6f03f634d26f79774368cc2620af
Yash <yashsaini18166@gmail.com>
2026-09-20 18:02:48 +0530

    Add home page route

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def home(request):
+    return HttpResponse('Django project is running.')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
